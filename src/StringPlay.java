544a65649cd7dfa1f704e928d733882e48d87f7c
HouSheng Chiang <houshengchiang@gmail.com>
2018-01-31 10:48:02 -0500

    modified

--- src/StringPlay.java
+++ src/StringPlay.java
@@ -1,3 +1,9 @@
+/*//////////////////////////////////////////////////////////////*/
+/* Author:		Hou Sheng Chiang 								*/
+/* Course:		CSC 151 4102 Java								*/
+/* Description: 	Lab 04	StringPlay							*/
+/*//////////////////////////////////////////////////////////////*/
+
 import java.util.*;
 public class StringPlay {
 
@@ -11,33 +17,32 @@ public class StringPlay {
 		
 		//check for leading,trailing spaces
 		if ( aLine.equals(aLine.trim()) == true ) {
-			System.out.println("the original String has no leading or trailing whitespace.");
+			System.out.println("The original String has no leading or trailing whitespace.");
 		}else {
-			System.out.println("the original String has leading or trailing whitespace.");
-			//aLine = aLine.trim();
+			System.out.println("The original String has leading or trailing whitespace.");
 		}
-		
-		System.out.println(aLine.length());
-		
+				
 		String first3 = aLine.substring(0, 3);
 		String last4 = aLine.substring(aLine.length() - 4);
 		String mid = aLine.substring(3 , aLine.length() - 4);
 		
 		//Swap the first three and last four characters of aLine and print the result.
-		System.out.println("First 3 and last 4 chars swapped: " + last4 + mid + first3);
+		System.out.println("First 3 and last 4 chars swapped: " +
+							last4 + mid + first3);
 		
 		//Print aLine in all upper case.
 		System.out.println("to upper case: " + aLine.toUpperCase());
 				
 		//Print the compareTo results of comparing aLine in all lower case with the original aLine.
 		int intCompStr = aLine.compareTo(aLine.toLowerCase());
-		System.out.println("compareTo lower case version: " + intCompStr);
+		System.out.println("compareTo lower case version: " +
+							intCompStr);
 				
 		//Print whether the first half of aLine is the same as the last half of aLine except for case.
 		String firstHalf = aLine.substring(0, (int)(aLine.length()/2)) ;
 		String lastHalf = aLine.substring( (int)(aLine.length()/2) );
-		System.out.println(firstHalf+"\n" + lastHalf);
-		System.out.println(firstHalf.equalsIgnoreCase(lastHalf));
-
+		System.out.println(firstHalf+ "\t" + lastHalf);
+		System.out.println("Does the first half equal the last half? " + 
+							firstHalf.equalsIgnoreCase(lastHalf));
 	}
 }
--- src/ZapDashes.java
+++ src/ZapDashes.java
@@ -1,29 +1,31 @@
+/*//////////////////////////////////////////////////////////////*/
+/* Author:		Hou Sheng Chiang 								*/
+/* Course:		CSC 151 4102 Java								*/
+/* Description: 	Lab 04	ZapDashes								*/
+/*//////////////////////////////////////////////////////////////*/
 //Use only String's indexOf, lastIndexOf and substring methods.
 import java.util.*;
 public class ZapDashes {
 
 	public static void main(String[] args) {
 		
-		
-		
-		System.out.println("Enter a phone number: ");
+		System.out.println("Enter a number includes 2 \"-\" ");
 		
 		//Scanner input = new Scanner(System.in);
 		//String str = input.nextLine();		// input
 		
-		String str = "12345--";
+		String str = "123-45-";
 		
-		System.out.println(str);				//output
+		System.out.print(str+"\t\t");				//output
 			
-		System.out.println(str.indexOf('-') + ',');		
-		System.out.println(str.lastIndexOf('-'));		
+		//System.out.println(str.indexOf('-') + ',');
+		//output = str.indexOf('-') + 44
+		//System.out.println(str.indexOf('-') + "," + str.lastIndexOf('-'));		
 		
-		System.out.println( str.substring(0, (str.indexOf('-'))) +
+		System.out.print( str.substring(0, (str.indexOf('-'))) +
 				str.substring(str.indexOf('-')+1, str.lastIndexOf('-')) +
 				str.substring(str.lastIndexOf('-')+1));
-		
-		//System.out.println(str);
-			
+					
 	}
 	
 }
